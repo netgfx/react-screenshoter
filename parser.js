15c1d095f2850cdaa83c84f9635af5e606df739e
Michalis Dobekidis <m.dobekidis@7linternational.com>
2024-04-23 14:38:28 +0300

    changed to es js

--- parser.js
+++ parser.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const ogs = require("open-graph-scraper");
+import express from "express";
+import * as ogs from "open-graph-scraper";
 
 const app = express();
 const port = 3002;
